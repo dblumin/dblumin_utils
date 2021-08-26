9f70080d2aaf2cd5e84accd596a0af94d390701f
dblumin <denis.blumin@onetick.com>
2021-08-26 11:51:33 -0400

    updated error parser to include grep file name with warning

--- error_parser.py
+++ error_parser.py
@@ -13,7 +13,7 @@ def main(error_delimeter, filelist):
     with open(filelist, 'r') as file_list, open('summary.csv', 'w+') as output_file, open('invalid_symbols.csv', 'w+') as symbol_file:
         output_file.seek(0)
         output_file.truncate(0)
-        output_file.write('#TYPE,INSTANCE,VENUE,DATE,COUNT,EXAMPLE')
+        output_file.write('#TYPE,INSTANCE,VENUE,DATE,COUNT,GREP_FILE,EXAMPLE')
         symbol_file.seek(0)
         symbol_file.truncate(0)
         symbol_file.write('#SYMBOL,VENUE,DATE')
@@ -94,7 +94,7 @@ def main(error_delimeter, filelist):
                             if origin_log not in error_dictionary[error_delimeter][key]:
                                 error_dictionary[error_delimeter][key][origin_log] = []
                             # add line to lowest dictionary layer
-                            error_dictionary[error_delimeter][key][origin_log].append([line, loader])
+                            error_dictionary[error_delimeter][key][origin_log].append([line, loader, input_file])
 
                             # if line contains invalid '::' in symbol name, write that to invalid_symbols.csv
                             if '::' in line:
@@ -129,7 +129,8 @@ def main(error_delimeter, filelist):
                     # print line with format:ERROR_TYPE,VENUE,DATE,COUNT,EXAMPLE_MESSAGE
                     for origin_key in error_type:
                         error_list = error_type[origin_key]
-                        output_file.write(f'\n\"{key}...\",{error_list[0][1]},{origin_key},{len(error_list)},\"{error_list[0][0].rstrip()}\"')
+                        output_file.write(f'\n\"{key}...\",{error_list[0][1]},{origin_key},'
+                                          f'{len(error_list)},{error_list[0][2]},\"{error_list[0][0].rstrip()}\"')
 
 
 if __name__ == '__main__':
